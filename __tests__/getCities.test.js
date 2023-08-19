f72eb55e00d49c6f643736a524c17ecafe23b9a9
Asyraf Hussin <asyrafhussin4@gmail.com>
2023-08-19 23:25:36 +0800

    Add getCities test

---
+++ __tests__/getCities.test.js
@@ -0,0 +1,27 @@
+const { getCities } = require("../index");
+
+describe("getCities", () => {
+  it("should be defined", () => {
+    expect(getCities).toBeDefined();
+  });
+
+  it("should return an array when a valid state is provided", () => {
+    const exampleState = "Johor";
+    expect(Array.isArray(getCities(exampleState))).toBe(true);
+  });
+
+  it("should return correct cities for a given state", () => {
+    const exampleState = "Johor";
+    const expectedCities = ["Johor Bahru", "Masai"];
+    const cities = getCities(exampleState);
+    expectedCities.forEach((city) => {
+      expect(cities).toContain(city);
+    });
+  });
+
+  it("should handle invalid states appropriately", () => {
+    const invalidState = "InvalidState";
+    const response = getCities(invalidState);
+    expect(response).toEqual([]);
+  });
+});
