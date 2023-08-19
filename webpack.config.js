053fa71108b64bc96a09256870274257d7f30186
Asyraf Hussin <asyrafhussin4@gmail.com>
2023-08-19 22:06:18 +0800

    Added support for direct script tag inclusion and provided minified version.

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require("path");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = [
+  {
+    mode: "development",
+    entry: "./index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "malaysia-postcodes.js",
+      library: "MalaysiaPostcodes",
+      libraryTarget: "umd",
+      umdNamedDefine: true,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+      ],
+    },
+  },
+
+  {
+    mode: "production",
+    entry: "./index.js",
+    output: {
+      path: path.resolve(__dirname, "dist"),
+      filename: "malaysia-postcodes.min.js",
+      library: "MalaysiaPostcodes",
+      libraryTarget: "umd",
+      umdNamedDefine: true,
+    },
+    optimization: {
+      minimize: true,
+      minimizer: [new TerserPlugin()],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          loader: "babel-loader",
+        },
+      ],
+    },
+  },
+];
