c21c6aad41198ee36fbd0a0a117fee496fda6964
Asyraf Hussin <asyrafhussin4@gmail.com>
2023-08-20 22:30:33 +0800

    Update webpack configuration and add copyright banner

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,68 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
+const webpack = require("webpack");
+const package = require("./package.json");
 
-module.exports = [
-  {
-    mode: "development",
-    entry: "./index.js",
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "malaysia-postcodes.js",
-      library: "malaysiaPostcodes",
-      libraryTarget: "umd",
-      umdNamedDefine: true,
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: "babel-loader",
-        },
-      ],
-    },
-  },
+const banner = `
+  malaysia-postcodes v${
+    package.version
+  } (https://github.com/AsyrafHussin/npm-malaysia-postcodes)
+  Copyright 2020-${new Date().getFullYear()} Asyraf Hussin
+  Licensed under ISC (https://github.com/AsyrafHussin/npm-malaysia-postcodes/blob/main/LICENSE)
+`;
 
-  {
-    mode: "production",
-    entry: "./index.js",
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "malaysia-postcodes.min.js",
-      library: "malaysiaPostcodes",
-      libraryTarget: "umd",
-      umdNamedDefine: true,
-    },
-    optimization: {
-      minimize: true,
-      minimizer: [new TerserPlugin()],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: "babel-loader",
-        },
-      ],
-    },
+const commonConfig = {
+  entry: "./index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    library: "malaysiaPostcodes",
+    libraryTarget: "umd",
+    umdNamedDefine: true,
   },
-];
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: banner,
+      raw: false,
+      entryOnly: true,
+    }),
+  ],
+};
+
+module.exports = () => {
+  return [
+    {
+      ...commonConfig,
+      mode: "development",
+      devtool: false,
+      output: {
+        ...commonConfig.output,
+        filename: "malaysia-postcodes.js",
+      },
+    },
+    {
+      ...commonConfig,
+      mode: "production",
+      output: {
+        ...commonConfig.output,
+        filename: "malaysia-postcodes.min.js",
+      },
+      optimization: {
+        minimize: true,
+        minimizer: [
+          new TerserPlugin({
+            extractComments: false,
+          }),
+        ],
+      },
+    },
+  ];
+};
