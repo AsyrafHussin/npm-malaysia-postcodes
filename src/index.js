d62f9c5f802e6af9d60d1268f4b0ac791c82290f
Asyraf Hussin <asyrafhussin4@gmail.com>
2023-08-25 16:56:35 +0800

    Refactor functions for clarity and add comprehensive function comments

--- src/index.js
+++ src/index.js
@@ -1,85 +1,73 @@
+// Load data from the JSON file
 const data = require("./data.json");
+
+// Extract all postcode data for the states
 const allPostcodes = data.state;
 
-// get all available states
+/**
+ * Retrieves the list of all states.
+ * @returns {Array} Array containing names of all states.
+ */
 const getStates = () => {
-  let states = [];
-
-  allPostcodes.map(state => {
-    states.push(state.name);
-
-    return null;
-  });
-
-  return states;
+  return allPostcodes.map((state) => state.name);
 };
 
-// get all avaialable city based on state
-const getCities = state => {
-  let cities = [];
-
-  allPostcodes.filter(stateFilter => {
-    if (stateFilter.name.toLowerCase() === state.toLowerCase()) {
-      const city = stateFilter.city;
-
-      city.map(cityFilter => {
-        cities.push(cityFilter.name);
-
-        return null;
-      });
-    }
-    return null;
-  });
-
-  return cities;
+/**
+ * Gets all cities for a given state.
+ * @param {string} selectedState - The name of the state for which cities are to be retrieved.
+ * @returns {Array} Array containing names of cities for the selected state. Empty array if the state is not found.
+ */
+const getCities = (selectedState) => {
+  const stateObj = allPostcodes.find(
+    (state) => state.name.toLowerCase() === selectedState.toLowerCase()
+  );
+  return stateObj ? stateObj.city.map((city) => city.name) : [];
 };
 
-// get all availabel postcodes based on city and state
+/**
+ * Retrieves all postcodes for a given state and city.
+ * @param {string} state - The name of the state.
+ * @param {string} city - The name of the city.
+ * @returns {Array} Array containing postcodes for the selected state and city. Empty array if not found.
+ */
 const getPostcodes = (state, city) => {
-  let postcodes = [];
-
-  allPostcodes.filter(stateFilter => {
-    if (stateFilter.name.toLowerCase() === state.toLowerCase()) {
-      const cities = stateFilter.city;
-
-      cities.filter(cityFilter => {
-        if (cityFilter.name.toLowerCase() === city.toLowerCase()) {
-          postcodes = cityFilter.postcode;
-        }
-
-        return null;
-      });
-    }
-    return null;
-  });
-
-  return postcodes;
+  const stateObj = allPostcodes.find(
+    (s) => s.name.toLowerCase() === state.toLowerCase()
+  );
+  if (stateObj) {
+    const cityObj = stateObj.city.find(
+      (c) => c.name.toLowerCase() === city.toLowerCase()
+    );
+    return cityObj ? cityObj.postcode : [];
+  }
+  return [];
 };
 
-// find state and city based on postcode
-const findPostcode = postcode => {
-  let result = {
-    found: false
-  };
-
-  allPostcodes.filter(state => {
-    state.city.map(city => {
+/**
+ * Finds the state and city based on a given postcode.
+ * @param {string} postcode - The postcode to search for.
+ * @returns {Object} An object containing 'found' (boolean), 'state' (string if found), and 'city' (string if found).
+ */
+const findPostcode = (postcode) => {
+  for (const state of allPostcodes) {
+    for (const city of state.city) {
       if (city.postcode.includes(postcode)) {
-        result.found = true;
-        result.state = state.name;
-        result.city = city.name;
+        return {
+          found: true,
+          state: state.name,
+          city: city.name,
+        };
       }
-      return null;
-    });
-    return null;
-  });
-
-  return result;
+    }
+  }
+  return { found: false };
 };
 
-// exports all variable and function
-module.exports.allPostcodes = allPostcodes;
-module.exports.getStates = getStates;
-module.exports.getCities = getCities;
-module.exports.getPostcodes = getPostcodes;
-module.exports.findPostcode = findPostcode;
+// Exporting the functions and data for external use
+module.exports = {
+  allPostcodes,
+  getStates,
+  getCities,
+  getPostcodes,
+  findPostcode,
+};
