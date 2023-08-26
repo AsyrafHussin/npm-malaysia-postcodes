1afea0e117c0f771298a23689fbbd2e22ea54f49
Asyraf Hussin <asyrafhussin4@gmail.com>
2023-08-27 02:52:01 +0800

    Move update-cdn-versions.js to scripts folder

--- scripts/update-cdn-versions.js
+++ scripts/update-cdn-versions.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-const { version } = require('./package.json');
+const { version } = require('../package.json');
 
 function updateCDNLinks(filename) {
   const content = fs.readFileSync(filename, 'utf8');
   const updatedContent = content.replace(/@(\d+\.\d+\.\d+)/g, `@${version}`);
-  fs.writeFileSync(filename, updatedContent, 'utf8');
+  fs.writeFileSync(`../${filename}`, updatedContent, 'utf8');
 }
 
 updateCDNLinks('README.md');
