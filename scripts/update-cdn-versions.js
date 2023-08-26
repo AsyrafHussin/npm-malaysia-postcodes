e3650a6e875f2575e6553a60e4dc099c82ec7cc0
Asyraf Hussin <asyrafhussin4@gmail.com>
2023-08-27 02:55:01 +0800

    Update cdn version scripts

--- scripts/update-cdn-versions.js
+++ scripts/update-cdn-versions.js
@@ -4,7 +4,7 @@ const { version } = require('../package.json');
 function updateCDNLinks(filename) {
   const content = fs.readFileSync(filename, 'utf8');
   const updatedContent = content.replace(/@(\d+\.\d+\.\d+)/g, `@${version}`);
-  fs.writeFileSync(`../${filename}`, updatedContent, 'utf8');
+  fs.writeFileSync(`${filename}`, updatedContent, 'utf8');
 }
 
 updateCDNLinks('README.md');
