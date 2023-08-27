ab0bff7ba56b982f1408ab9b9a80e1f7ea81aa57
Asyraf Hussin <asyrafhussin4@gmail.com>
2023-08-27 20:48:42 +0800

    Update autocomplete example at es6

--- examples/es6/autocomplete/main.js
+++ examples/es6/autocomplete/main.js
@@ -1,21 +1,52 @@
 import { getPostcodesByPrefix } from 'malaysia-postcodes';
 
+function generateOptions(matchingPostcodes) {
+  return matchingPostcodes.map(postcode => {
+    const option = document.createElement('div');
+    option.className = 'custom-option';
+    option.innerHTML = postcode;
+    option.addEventListener('click', function () {
+      const postcodeInput = document.getElementById('postcodeInput');
+      postcodeInput.value = this.innerHTML;
+      hideDropdown();
+    });
+    return option;
+  });
+}
+
+function hideDropdown() {
+  const dropdown = document.getElementById('customDropdown');
+  dropdown.innerHTML = '';
+  dropdown.style.display = 'none';
+}
+
+function showDropdown(elements) {
+  const dropdown = document.getElementById('customDropdown');
+  dropdown.innerHTML = '';
+  dropdown.style.display = 'block';
+  elements.forEach(element => dropdown.appendChild(element));
+}
+
 function updateDatalist() {
-  const input = document.getElementById('postcodeInput').value;
-  const dataList = document.getElementById('postcodes');
-  dataList.innerHTML = '';
+  const postcodeInput = document.getElementById('postcodeInput');
+  const input = postcodeInput.value;
 
-  const matchingPostcodes = getPostcodesByPrefix(input);
+  if (input.length >= 1 && input.length <= 5) {
+    const matchingPostcodes = getPostcodesByPrefix(input);
 
-  for (const postcode of matchingPostcodes) {
-    const option = document.createElement('option');
-    option.value = postcode;
-    dataList.appendChild(option);
+    if (matchingPostcodes.length === 0) {
+      hideDropdown();
+      return;
+    }
+
+    const options = generateOptions(matchingPostcodes);
+    showDropdown(options);
+  } else {
+    hideDropdown();
   }
 }
 
 document.addEventListener('DOMContentLoaded', event => {
-  document
-    .getElementById('postcodeInput')
-    .addEventListener('input', updateDatalist);
+  const postcodeInput = document.getElementById('postcodeInput');
+  postcodeInput.addEventListener('input', updateDatalist);
 });
